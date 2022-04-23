850f37dc06e16f7a0045138121d3136e1a3918a6
Nielotz <mati2806@poczta.fm>
2022-04-23 20:48:35 +0200

    Add info, simplify param name

--- C++/OpenGL/Learning/src/headers/logging/logging.h
+++ C++/OpenGL/Learning/src/headers/logging/logging.h
@@ -3,10 +3,17 @@
 
 namespace logging
 {
-	void error(const std::string& category, const std::string& errorMessage);
+	void error(const std::string& category, const std::string& message);
 
-	void error(const std::string& errorMessage);
+	void error(const std::string& message);
 
-	void warning(const std::string& category, const std::string& errorMessage);
+	void warning(const std::string& category, const std::string& message);
 
+	void info(const std::string& category, const std::string& message);
+
+	template <typename T>
+	void info(const std::string& category, const T& data)
+	{
+		std::cout << "[INFO] " << "(" << category << ") " << data << std::endl;
+	}
 }
