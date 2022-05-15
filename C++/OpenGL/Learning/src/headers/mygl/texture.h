9db630b7d0e80e4ad6f819edff71a2f77af03d88
Nielotz <mati2806@poczta.fm>
2022-05-15 15:40:49 +0200

    Extend texture

--- C++/OpenGL/Learning/src/headers/mygl/texture.h
+++ C++/OpenGL/Learning/src/headers/mygl/texture.h
@@ -4,17 +4,44 @@
 
 #include <string>
 
-class MyGLTexture
+namespace MyGL
 {
-	const std::string path;
-	int width = 0;
-	int height = 0;
-	unsigned int textureID = -1;
+	class Texture
+	{
+	public:
+		virtual unsigned int getTextureID() const = 0;
 
-public:
-	unsigned int getTextureID() const;
-	MyGLTexture(const std::string& path);
-	void bind(size_t slot);
-	void unbind();
-	~MyGLTexture();
-};
+		/// <summary>
+		/// Create texture object, and read data from file.
+		/// </summary>
+		/// <param name="path">path to the file</param>
+		virtual void bind(unsigned int slot = 0) = 0;
+		virtual void unbind() = 0;
+		int getSlot() const;
+
+	protected:
+		Texture(const std::string& path);
+		const std::string& path;
+		int width = -1;
+		int height = -1;
+		int bpp = -1;
+		unsigned int textureID_GL = -1;
+		int slot = -1;
+	};
+
+	class Texture2D : public Texture
+	{
+	public:
+		unsigned int getTextureID() const;
+
+		Texture2D(const std::string& path);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="slot">preferably keep it under 16 for sure under 32</param>
+		void bind(unsigned int slot = 0) override;
+		void unbind() override;
+
+		~Texture2D();
+	};
+}
